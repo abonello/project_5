856152461e7379a2751281f27d596d02f8a74243
abonello <ab@anthonybonello.co.uk>
2019-02-03 17:00:04 +0000

    Try adding a media folder with related variables in setting.py

--- UniQueCorn/settings.py
+++ UniQueCorn/settings.py
@@ -163,6 +163,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # This is needed for working on cloud 9 due to a cookie bug
 # We need to override the default message storage
