55ed63797d39b9e1c7a1cac7aec73ad002a673c8
abonello <ab@anthonybonello.co.uk>
2019-02-03 16:47:13 +0000

    Set Debug to automatically switch between True and false

--- UniQueCorn/settings.py
+++ UniQueCorn/settings.py
@@ -33,8 +33,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-# DEBUG = development
+# DEBUG = True
+DEBUG = development
 
 ALLOWED_HOSTS = [
     # "localhost",
