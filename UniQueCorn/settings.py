5bc3cdd646d4cdf25d67beb27352290b7ae370a9
abonello <ab@anthonybonello.co.uk>
2019-02-03 12:42:31 +0000

    Try for heroku again

--- UniQueCorn/settings.py
+++ UniQueCorn/settings.py
@@ -159,9 +159,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
 
 # This is needed for working on cloud 9 due to a cookie bug
 # We need to override the default message storage
