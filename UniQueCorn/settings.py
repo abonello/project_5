e02aeba89b9038805abb0632b3c713cf19af0fa4
abonello <ab@anthonybonello.co.uk>
2019-02-03 13:35:55 +0000

    Try setting STATIC_ROOT

--- UniQueCorn/settings.py
+++ UniQueCorn/settings.py
@@ -159,10 +159,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # This is needed for working on cloud 9 due to a cookie bug
