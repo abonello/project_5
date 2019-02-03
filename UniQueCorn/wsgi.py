8064815733a37fab942704004e456f2971f6d254
abonello <ab@anthonybonello.co.uk>
2019-02-03 15:40:08 +0000

    Fixed wsgi.py

--- UniQueCorn/wsgi.py
+++ UniQueCorn/wsgi.py
@@ -15,4 +15,4 @@ from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "UniQueCorn.settings")
 
 application = get_wsgi_application()
-# application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(application)
