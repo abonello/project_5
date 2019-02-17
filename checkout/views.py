113db67a999ea2d38a626e692786fe1cc067284c
abonello <ab@anthonybonello.co.uk>
2019-02-17 19:20:45 +0000

    heroku testing

--- checkout/views.py
+++ checkout/views.py
@@ -34,45 +34,49 @@ def checkout(request):
         else:
             print("Payment form is NOT valid")
 
-        if order_form.is_valid() and payment_form.is_valid():
-            order = order_form.save(commit=False)
-            order.date = timezone.now()
-            order.save()
+        try: 
+            if order_form.is_valid() and payment_form.is_valid():
+                order = order_form.save(commit=False)
+                order.date = timezone.now()
+                order.save()
 
-            cart = request.session.get('cart', {})
-            total = 0
+                cart = request.session.get('cart', {})
+                total = 0
 
-            for id, quantity in cart.items():
-                product = get_object_or_404(Product, pk=id)
-                total += quantity * product.price
-                order_line_item = OrderLineItem(
-                    order = order,
-                    product = product,
-                    quantity = quantity
+                for id, quantity in cart.items():
+                    product = get_object_or_404(Product, pk=id)
+                    total += quantity * product.price
+                    order_line_item = OrderLineItem(
+                        order = order,
+                        product = product,
+                        quantity = quantity
+                        )
+                    order_line_item.save()
+
+                try: 
+                    # stripe works with cents or pence
+                    customer = stripe.Charge.create(
+                        amount = int(total * 100),
+                        currency = "EUR",
+                        description = request.user.email,
+                        card = payment_form.cleaned_data['stripe_id'],
                     )
-                order_line_item.save()
+                except stripe.error.CardError:
+                    messages.error(request, "Your card was declined!")
 
-            try: 
-                # stripe works with cents or pence
-                customer = stripe.Charge.create(
-                    amount = int(total * 100),
-                    currency = "EUR",
-                    description = request.user.email,
-                    card = payment_form.cleaned_data['stripe_id'],
-                )
-            except stripe.error.CardError:
-                messages.error(request, "Your card was declined!")
-
-            if customer.paid:
-                messages.error(request, "You have successfully paid.")
-                # Empty cart
-                request.session['cart'] = {}
-                return redirect(reverse('products'))
-            else: 
-                messages.error(request, "Unable to take payment.")
-        else:
-            print(payment_form.errors)
-            messages.error(request, "We were unable to take a payment with that card!")
+                if customer.paid:
+                    messages.error(request, "You have successfully paid.")
+                    # Empty cart
+                    request.session['cart'] = {}
+                    return redirect(reverse('products'))
+                else: 
+                    messages.error(request, "Unable to take payment.")
+            else:
+                print(payment_form.errors)
+                messages.error(request, "We were unable to take a payment with that card!")
+        except:
+            print("Is this where the error is?")
+        
     else:
         # Return a blank form
         payment_form = MakePaymentForm()
