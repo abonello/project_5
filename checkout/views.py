a1ffa791330217f4e6ee66720d109a35ab9beca5
abonello <ab@anthonybonello.co.uk>
2019-02-17 19:04:40 +0000

    Testing heroku

--- checkout/views.py
+++ checkout/views.py
@@ -18,21 +18,21 @@ stripe.api_key = settings.STRIPE_SECRET
 @login_required()
 def checkout(request):
     if request.method == "POST":
-        # print("View received a POST request")
+        print("View received a POST request")
         try:
             order_form = OrderForm(request.POST)
             payment_form = MakePaymentForm(request.POST)
         except:
             print("Error occured")
 
-        # if order_form.is_valid():
-        #     print("Order form is valid")
-        # else:
-        #     print("Order form is NOT valid")
-        # if payment_form.is_valid():
-        #     print("Payment form is valid")
-        # else:
-        #     print("Payment form is NOT valid")
+        if order_form.is_valid():
+            print("Order form is valid")
+        else:
+            print("Order form is NOT valid")
+        if payment_form.is_valid():
+            print("Payment form is valid")
+        else:
+            print("Payment form is NOT valid")
 
         if order_form.is_valid() and payment_form.is_valid():
             order = order_form.save(commit=False)
