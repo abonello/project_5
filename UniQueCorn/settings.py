f36957398837c4eadc93bcde8cf965b5ee9b059a
abonello <ab@anthonybonello.co.uk>
2019-02-17 14:08:20 +0000

    Changed code for accessing database for Travis to use sqlite

--- UniQueCorn/settings.py
+++ UniQueCorn/settings.py
@@ -114,9 +114,18 @@ if development:
         }
     }
 else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
+    if "DATABASE_URL" in os.environ:
+        DATABASES = {
+            'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        }
+    else:
+        print("DATABASE_URL not found - using SQlite instead.")
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            }
+        }
 
 
 
