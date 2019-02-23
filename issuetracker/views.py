2636bfdd2e2d0183296515a0285df9411ae2f150
abonello <ab@anthonybonello.co.uk>
2019-02-23 09:10:31 +0000

    Mockup of data and render

--- issuetracker/views.py
+++ issuetracker/views.py
@@ -4,5 +4,33 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required()
 def issuetracker(request):
-    return render(request, "issue_tracker.html", {})
+    issues = [
+        {
+            'id': '1',
+            'title': "First Feature Request",
+            'description': "I need this functionality added that when I press the button it goes in turbo mode and produces double the amount of corn",
+            'posted_by': "AlfaAlfa",
+            'votes': "5"
+        },
+        {
+            'id': '2',
+            'title': "Popcorn maker",
+            'description': "I would like the app to pop the corns ready to eat. I am fed up having to cook them.",
+            'posted_by': "Bastard",
+            'votes': "20"
+        },
+        {
+            'id': '3',
+            'title': "Colored Corn",
+            'description': "I would like to be able to choose the color of the corn produced, nut just the boring natural color.",
+            'posted_by': "Complainer",
+            'votes': "1"
+        }
+
+    ]
+    for each in issues:
+        print(each)
+
+    # print(issues.values)
+    return render(request, "issue_tracker.html", {'issues': issues})
 
