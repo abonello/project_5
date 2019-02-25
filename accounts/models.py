f8442aa98660d717906689ab403aba048181f8c3
abonello <ab@anthonybonello.co.uk>
2019-02-25 12:39:31 +0000

    Added UserCoins model in accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
+import datetime
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserCoins(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='relateduser')
+    coin_amount = models.PositiveSmallIntegerField(default=0)
+
+
+    class Meta:
+        verbose_name_plural = "User coins"
+
+    def __str__(self):
+        return "{} has {} coins".format(self.user, self.coin_amount)
