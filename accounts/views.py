b1c3ec11e596f92154f217fb7bc0d33126e78426
abonello <ab@anthonybonello.co.uk>
2019-03-03 18:14:25 +0000

    Login redirects to home page.

--- accounts/views.py
+++ accounts/views.py
@@ -28,11 +28,11 @@ def login(request):
             user = auth.authenticate(
                     username=request.POST['username'],
                     password=request.POST['password'])
-            
             if user:
                 auth.login(user=user, request=request)
                 messages.success(request, "You have successfully logged in.")
-                return redirect(reverse('todo'))
+                # return redirect(reverse('todo'))
+                return redirect(reverse('index'))
             else:
                 login_form.add_error(None, "Your username or password is incorrect.")
     else:
