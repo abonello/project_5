eab981ee737c0fe5023c227c3aba67943ce78886
abonello <ab@anthonybonello.co.uk>
2019-03-05 07:58:46 +0000

    When registering a new user I create coins for that user.

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from accounts.forms import UserLoginForm, UserRegistrationForm
+from .models import UserCoins
 
 def index(request):
     """Return the index.html file"""
@@ -58,6 +59,11 @@ def registration(request):
             )
 
             if user:
+                print(user)
+                coins = UserCoins()
+                coins.user = user
+                coins.save()
+                print(coins)
                 auth.login(user=user, request=request)
                 messages.success(request, "You have successfully registered.")
                 # return redirect(reverse('todo'))
