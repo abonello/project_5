229ee07cd0395a7db4434fa157b7f33f85730a25
abonello <ab@anthonybonello.co.uk>
2019-03-17 09:55:45 +0000

    Some automated tests

--- home/tests_home_views.py
+++ home/tests_home_views.py
@@ -1,40 +1,38 @@
 from django.test import TestCase
-
 from django.shortcuts import get_object_or_404
 # from .models import Item
 # from django.contrib.auth.models import User
 
 
-
 class TestViews(TestCase):
-    '''
-        1 test: we are going to the correct location (correct url)
-        2.test: status code
-        3.test: correct template is used
-    '''
+        '''
+                1 test: we are going to the correct location (correct url)
+                2.test: status code
+                3.test: correct template is used
+        '''
 
-    def test_get_home_page(self):
-            # fake a request to the url we pass as an argument
-            page = self.client.get('/')
-            self.assertEqual(page.status_code, 200)
-            self.assertTemplateUsed(page, "index_home.html")
+        def test_get_home_page(self):
+                # fake a request to the url we pass as an argument
+                page = self.client.get('/')
+                self.assertEqual(page.status_code, 200)
+                self.assertTemplateUsed(page, "index_home.html")
 
-    def test_get_about_page(self):
-            page = self.client.get('/about/')
-            self.assertEqual(page.status_code, 200)
-            self.assertTemplateUsed(page, "about.html")
+        def test_get_about_page(self):
+                page = self.client.get('/about/')
+                self.assertEqual(page.status_code, 200)
+                self.assertTemplateUsed(page, "about.html")
 
-    def test_get_blog_page(self):
-            page = self.client.get('/blog/')
-            self.assertEqual(page.status_code, 200)
-            self.assertTemplateUsed(page, "blog.html")
+        def test_get_blog_page(self):
+                page = self.client.get('/blog/')
+                self.assertEqual(page.status_code, 200)
+                self.assertTemplateUsed(page, "blog.html")
 
-    def test_get_uqc_app_page(self):
-            page = self.client.get('/uqc_app/')
-            self.assertEqual(page.status_code, 200)
-            self.assertTemplateUsed(page, "uqc-app.html")
+        def test_get_uqc_app_page(self):
+                page = self.client.get('/uqc_app/')
+                self.assertEqual(page.status_code, 200)
+                self.assertTemplateUsed(page, "uqc-app.html")
 
-    def test_get_contact_page(self):
-            page = self.client.get('/contact/')
-            self.assertEqual(page.status_code, 200)
-            self.assertTemplateUsed(page, "contact.html")
+        def test_get_contact_page(self):
+                page = self.client.get('/contact/')
+                self.assertEqual(page.status_code, 200)
+                self.assertTemplateUsed(page, "contact.html")
---
+++ issuetracker/tests_issuetracker_views.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from django.shortcuts import get_object_or_404
+from .models import Issue, IssueComment
+from django.contrib.auth.models import User
+
+
+# class TestViews(TestCase):
+'''
+    1 test: we are going to the correct location (correct url)
+    2.test: status code
+    3.test: correct template is used
+'''
+
+    # def test_get_home_page(self):
+        # # fake a request to the url we pass as an argument
+        # page = self.client.get('/')
+        # self.assertEqual(page.status_code, 200)
+        # self.assertTemplateUsed(page, "index_home.html")
+
+    # def test_get_issues_page(self):
+
+
+    # def create_and_log_in_user(self):
+    #     test_user = User.objects.create_user(
+    #         username="test", email="test@example.com", password="testpword")
+    #     self.client.login(username='test', password='testpword')
